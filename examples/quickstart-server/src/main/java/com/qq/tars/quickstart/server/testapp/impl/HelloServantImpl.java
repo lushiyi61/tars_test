b62a1f51f16e3a11a96a9ce932a7bd9ce62842e4
Jane <iwof920125@qq.com>
2018-07-06 14:49:11 +0800

    确认可以对接kotlin

--- examples/quickstart-server/src/main/java/com/qq/tars/quickstart/server/testapp/impl/HelloServantImpl.java
+++ examples/quickstart-server/src/main/java/com/qq/tars/quickstart/server/testapp/impl/HelloServantImpl.java
@@ -17,11 +17,13 @@
 package com.qq.tars.quickstart.server.testapp.impl;
 
 import com.qq.tars.quickstart.server.testapp.HelloServant;
+import com.qq.tars.quickstart.server.testapp.impl.TestKt;
 
 public class HelloServantImpl implements HelloServant {
 
     @Override
     public String hello(int no, String name) {
-        return String.format("hello no=%s, name=%s, time=%s", no, name, System.currentTimeMillis());
+//        TestKt.demo();
+        return String.format("hello no=%s, name=%s, time=%s kotlin=%s", no, name, System.currentTimeMillis(),TestKt.demo());
     }
 }
---
+++ examples/quickstart-server/src/main/java/com/qq/tars/quickstart/server/testapp/impl/test.kt
@@ -0,0 +1,10 @@
+package com.qq.tars.quickstart.server.testapp.impl
+
+import java.util.*
+
+fun demo(): String {
+//    val list = ArrayList<String>()
+//    list.add("hello world")
+    println("test！！！ demo");
+    return "hello world";
+}
